d6cf12b76c56b75754417624a5199ca3535bd2d6
SamGrobelny <samgrobelny@gmail.com>
2021-12-04 13:17:47 -0500

    day 2 complete

--- day2.py
+++ day2.py
@@ -14,8 +14,27 @@ def control_sub(filename):
                 depth += amount
         return depth * horizontal
 
+def control_sub_accurate(filename):
+    with open(filename) as file:
+        horizontal = 0
+        depth = 0
+        curr_aim = 0
+        for line in file:
+            line_arr = line.strip().split()
+            command = line_arr[0]
+            amount = int(line_arr[1])
+            if command == 'forward':
+                horizontal += amount
+                depth += curr_aim * amount
+            elif command == 'up':
+                curr_aim -= amount
+            elif command == 'down':
+                curr_aim += amount
+    return depth * horizontal
+
 def main():
-    print(control_sub('data/day-2-input.txt'))
+    # print(control_sub('data/day-2-input.txt'))
+    print(control_sub_accurate('data/day-2-input.txt'))
 
 if __name__ == '__main__':
     main()
