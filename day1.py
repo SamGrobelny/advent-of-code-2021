c0ee36ffa5484758c01ff225071a0b2a01ed09a2
SamGrobelny <samgrobelny@gmail.com>
2021-12-04 12:58:46 -0500

    day 1 part 2 complete

--- day1.py
+++ day1.py
@@ -9,8 +9,33 @@ def measure_depth(filename):
             prev = stripped
         return num_inc
 
+def sliding_measure(filename):
+    with open(filename) as file:
+        inc_count = 0
+        three_one = []
+        three_two = []
+        three_one_sum = 0
+        three_two_sum = 0
+        for i in range(3):
+            three_one.append(int(next(file).strip()))
+            three_one_sum += three_one[i]
+        for line in file:
+            stripped = line.strip()
+            three_two = list(three_one)
+            three_two.pop(0)
+            three_two.append(int(stripped))
+            three_two_sum = sum(three_two)
+            if three_two_sum > three_one_sum:
+                inc_count += 1
+            three_one = list(three_two)
+            three_one_sum = sum(three_one)
+    return inc_count
+
+        
+
 def main():
-    print(measure_depth('data/day-1-input.txt'))
+    # print(measure_depth('data/day-1-input.txt'))
+    print(sliding_measure('data/day-1-input.txt'))
 
 if __name__ == '__main__':
     main()
